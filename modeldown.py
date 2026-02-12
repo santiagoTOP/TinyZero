38dec1414b03791c1c9274bc1c12471bb2c17328
santiagoTOP <1537211712top@gmail.com>
2026-02-12 11:01:05 +0800

    modified:   modeldown.py 	modified:   scripts/train_tiny_zero.sh

--- modeldown.py
+++ modeldown.py
@@ -1,3 +1,3 @@
 from modelscope.hub.snapshot_download import snapshot_download
-snapshot_download(model_id="Qwen/Qwen3-0.6B", cache_dir="/root/autodl-tmp/models")
+snapshot_download(model_id="Qwen/Qwen2.5-1.5B-Instruct", cache_dir="/root/autodl-tmp/models")
 
