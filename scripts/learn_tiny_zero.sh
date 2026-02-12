18c0484e7fd147cfe73bd6123d1de142adf1d25a
TNG PNG <1537211712top@gmail.com>
2026-02-12 08:54:12 +0800

    new file:   scripts/learn_tiny_zero.sh 	modified:   scripts/train_tiny_zero.sh

--- scripts/learn_tiny_zero.sh
+++ scripts/learn_tiny_zero.sh
@@ -1,15 +1,15 @@
 python3 -m verl.trainer.main_ppo \
 data.train_files=/root/autodl-tmp/TinyZero/data/train.parquet \
 data.val_files=/root/autodl-tmp/TinyZero/data/test.parquet \
-data.train_batch_size=256 \
-data.val_batch_size=1312 \
+data.train_batch_size=1 \
+data.val_batch_size=1 \
 data.max_prompt_length=256 \
-data.max_response_length=1024 \
+data.max_response_length=256 \
 actor_rollout_ref.model.path=/root/autodl-tmp/models/Qwen/Qwen2.5-0.5B-Instruct \
 actor_rollout_ref.model.use_remove_padding=True \
 actor_rollout_ref.actor.use_dynamic_bsz=True \
 actor_rollout_ref.actor.optim.lr=1e-6 \
-actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+actor_rollout_ref.actor.ppo_mini_batch_size=8 \
 actor_rollout_ref.actor.ppo_micro_batch_size=8 \
 actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
